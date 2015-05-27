f3c28c70334e1db99c7bf689030ae6d7c0610478
cgaglio <cgaglio@antidot.net>
2015-05-27 21:42:43 +0200

    node number can be used to filter data to get

--- arduino.py
+++ arduino.py
@@ -5,7 +5,7 @@ import serial
 import os
 
 class ArduinoReadSerial:
-   def __init__(self,dataToGet,shift=2):
+   def __init__(self,dataDefinition,shift=2):
       self.plugin_name = 'arduino'
       self.speed = 57600
       self.device = '/dev/ttyUSB1'
@@ -13,7 +13,7 @@ class ArduinoReadSerial:
       self.debug = False
       self.timeout = 1
       self.plugin_instance = None
-      self.dataToGet = dataToGet
+      self.dataDefinition = dataDefinition
       self.shift = shift
       self.lastValues = {}
 
@@ -106,7 +106,7 @@ class ArduinoReadSerial:
 	 except ValueError as ve:
 	    self.log_warning(str(ve))
 	    continue
-         line = str(bytesLine).replace('\0','')
+         line = (str(lineSplitted[1]) + ' ' + str(bytesLine)).replace('\0','')
          lineSplitted = line.split()
          self.log_debug(line)
          self.add_values(lineSplitted,values)
@@ -119,14 +119,19 @@ class ArduinoReadSerial:
 
    def add_values(self,lineSplitted,values):
       lineSplittedSize = len(lineSplitted)
-      for key in self.dataToGet.keys():
-         position = self.dataToGet[key]
-         if not lineSplittedSize >= position:
+      # check that node number exist in dataDefinition
+      if lineSplitted[0] not in self.dataDefinition:
+	 self.log_warning('node number %s not in data definition' % (lineSplitted[0]))
+	 return
+      dataToGet = self.dataDefinition[lineSplitted[0]]
+      for key in dataToGet.keys():
+         position = dataToGet[key]
+         if not lineSplittedSize >= (position + 1):
             continue
          if not key in values:
             values[key] = []
          try:
-            values[key].append(int(lineSplitted[position - 1]))
+            values[key].append(int(lineSplitted[position]))
          except ValueError as ve:
             self.log_warning(str(ve))
 
@@ -142,10 +147,21 @@ class ArduinoReadSerial:
          metric.type = 'gauge'
          metric.type_instance = key
          metric.values = [reduce(lambda x, y: x + y,value) / len(value)]
-         metric.dispatch()       
+         metric.dispatch()
 
-dataToGet = { 'Puissance apparente':2, 'Heures Creuses':3, 'Heures pleines':4, 'tension':5, 'Compteur production':7, 'RAM libre':8 }
-arduino = ArduinoReadSerial(dataToGet)
+
+# name and postion of the data to get from every line read from ttyUSB
+# it must contains the node number and the data for this node number
+# exemple: dataDefinition = { '2' : { 'tension':2 } ,
+#                             '3' : { 'temperature':1 }
+#                           } 
+#          lines = OK 2 3 4
+#                  OK 3 30
+#          tension will be 4
+#          temperature will be 30
+
+dataDefinition = { '2' : {'Puissance apparente':2, 'Heures Creuses':3, 'Heures pleines':4, 'tension':5, 'Compteur production':7, 'RAM libre':8 } }
+arduino = ArduinoReadSerial(dataDefinition)
 #== Hook Callbacks, Order is important! ==#
 collectd.register_config(arduino.config,name=arduino.plugin_name)
 collectd.register_init(arduino.init)
